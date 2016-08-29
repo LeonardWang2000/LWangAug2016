1498e13b976dada6e74332fc66bac06e685e58be
LeonardWang2000 <18wangl64@millshighsch.org>
2016-08-29 08:32:39 -0700

    Hello World Demonstration 8/29/16

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author Leonard Wang 
+ * @version 8/29/16
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Leonard Wang"); //Display the string
+    }
+}
+
