01430cc9a91be121d44e97c9e83330c4f733132e
LeonardWang2000 <18wangl64@millshighsch.org>
2016-08-30 08:28:47 -0700

    TestCode for Jar File demo

---
+++ TestJar/src/TestCode.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Leonard Wang
+ * Used to show how to create a jar file that I can turn in to Classroom
+ * August 30th, 2016
+ */ 
+public class TestCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
